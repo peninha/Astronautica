702f9f6e59bc229cd24e08bb4f55848b14339e48
Roberto "Pena" Spinelli <penadoxo@gmail.com>
2024-12-04 06:36:56 -0300

    fix

--- example2.18.py
+++ example2.18.py
@@ -17,13 +17,15 @@ v = 10.9148 # [km/s]
 
 three_body = Three_body_restricted(m1=M_earth, m2=M_moon, r12=r12, body1radius=Earth_radius, body2radius=Moon_radius)
 
-
-
 r_vec_0 = np.array([-three_body.pi2*three_body.r12, -(d + Earth_radius), 0])
 v_vec_0 = np.array([v*np.cos(np.radians(20)), -v*np.sin(np.radians(20)), 0])
 
-sol = three_body.trajectory(r_vec_0, v_vec_0, t_span=(0, 3.16689*86400), plot=True)
+sol = three_body.trajectory(r_vec_0, v_vec_0, t_span=(0, 3.16689*86400), t_eval=np.linspace(0, 3.16689*86400, 1000), plot=True)
 
 # Obter posição final
 r_final = sol.y[:3, -1]
 print(f"\nPosição final (x,y,z) = ({r_final[0]:.2f}, {r_final[1]:.2f}, {r_final[2]:.2f}) km")
+# Calcular a distância até a Lua
+r_moon = np.array([(1-three_body.pi2)*three_body.r12, 0, 0])
+dist_to_moon = np.linalg.norm(r_final - r_moon) - Moon_radius
+print(f"\nAltitude em relação à Lua = {dist_to_moon:.2f} km")
